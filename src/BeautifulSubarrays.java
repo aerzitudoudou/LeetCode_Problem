62f47bd398a2f66f6c6b2bc50003b2fd481d80a2
brend <brenda.zhang2016@gmail.com>
2020-04-25 15:26:45 -0400

    lint code 周赛 7: beautiful subarrays

--- src/BeautifulSubarrays.java
+++ src/BeautifulSubarrays.java
@@ -25,8 +25,12 @@ Explanation: No odd number in array
 *
 * */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeautifulSubarrays {
     //my way
+    //T: (n^2) s:O(1)
     public int BeautifulSubarrays(int[] nums, int numOdds) {
         int res = 0;
         int count = 0;
@@ -56,6 +60,38 @@ public class BeautifulSubarrays {
         return t % 2 == 1;
     }
 
-    //other's way : 中心开花
-    //TODO: 总结
+    //other's way : 中心开花 T: O（）
+    //抽象出奇数的index并加上边界用来计算n个奇数定义出的interval往左往右可形成数组的个数  最终的数组的个数 = total of (每一个interval向左可形成的数组的个数 * 这个interval向右可形成的数组的个数）
+    //way2
+    //                                       e.g. nums: [7, 8, 9, 10, 11]
+    //                                     num's index: [0, 1, 2,  3,  4]
+    //                                                   odd   odd    odd
+    //抽象出奇数Index 数组：                              [0,    2,      4]
+    //+左边界: -1 右边界 = nums.size = 5   tmp      [-1,  0,     2,     4,   5]
+    //                                                   |inter 1|
+    //                                             --a---         --b---
+    //init                                               i = 1
+    //计算a 段（左边可形成数组个数）*b 段             a =  tmp[i] - tmp[i-1]   b = tmp[i + k] - tmp[i]   k is the defined number of odds
+    public int BeautifulSubarrays2(int[] nums, int numOdds) {
+        int res = 0;
+        //build tmp
+        List<Integer> tmp = new ArrayList<>();
+        tmp.add(-1);
+        for(int i = 0; i < nums.length; i++){
+            if(isOdd(nums[i])){
+                tmp.add(i);
+            }
+        }
+        tmp.add(nums.length);
+
+
+        for(int i = 1; i + numOdds < tmp.size(); i++){
+            //caculate a * b
+            res += (tmp.get(i) - tmp.get(i - 1)) * (tmp.get(i + numOdds) - tmp.get(i + numOdds - 1));
+
+        }
+        return res;
+    }
+
+
 }
