423f0391db57e8595de035deb0164ee4bd3bbf0a
brend <brenda.zhang2016@gmail.com>
2020-06-20 14:49:02 -0400

    Longest common subsequence_滚动数组方法

--- src/LongestCommonSubsequence.java
+++ src/LongestCommonSubsequence.java
@@ -18,6 +18,9 @@ S = “abcde”, T = “cbabdfe”, the longest common subsequence of s and t is
 * -- char一样的, m[i - 1][j - 1] + 1
 * -- char不一样的， 取m[i - 1][j] 和m[i][j - 1]的最大值继承过来
 *
+* 这篇解答很好：
+* --https://leetcode.com/problems/longest-common-subsequence/solution/
+*
 *
 *
 *
@@ -49,6 +52,68 @@ public class LongestCommonSubsequence {
         return m[source.length()][target.length()];
     }
 
-    //way2: TODO: 滚动数组把空间复杂度降到O(min(m, n))
+    //way2: 滚动数组把空间复杂度降到O(min(m, n)): https://leetcode.com/problems/longest-common-subsequence/solution/
+    public int longest2(String source, String target) {
+        if(source == null || source.length() == 0 || target == null || target.length() == 0){
+            return 0;
+        }
+        //let source always be the shorter string
+        if(source.length() > target.length()){
+            String tmp = source;
+            source = target;
+            target = tmp;
+        }
 
+        int[] previous = new int[source.length() + 1];
+        int[] cur = null;
+        for(int i = 1; i <= target.length(); i++){
+            //每次create 一个新的cur
+            cur = new int[source.length() + 1];
+            for(int j = 1; j <= source.length(); j++){
+                if(source.charAt(j - 1) == target.charAt(i - 1)){
+                    cur[j] = previous[j - 1] + 1;
+                }else{
+                    cur[j] = Math.max(previous[j], cur[j - 1]);
+                }
+            }
+            previous = cur;
+        }
+        return cur[cur.length - 1];
+
+
+    }
+
+
+    //way3: 另一种方法实现滚动数组 - 滚动数组把空间复杂度降到O(min(m, n))
+    public int longest3(String source, String target) {
+        if(source == null || source.length() == 0 || target == null || target.length() == 0){
+            return 0;
+        }
+        //let source always be the shorter string
+        if(source.length() > target.length()){
+            String tmp = source;
+            source = target;
+            target = tmp;
+        }
+        //只create一次cur
+        int[] prev = new int[source.length() + 1];
+        int[] cur = new int[source.length() + 1];
+        for(int i = 1; i <= target.length(); i++){
+            for(int j = 1; j <= source.length(); j++){
+                if(source.charAt(j - 1) == target.charAt(i - 1)){
+                    cur[j] = prev[j - 1] + 1;
+                }else{
+                    cur[j] = Math.max(prev[j], cur[j - 1]);
+                }
+            }
+            //prev cur 互换，下一轮cur 直接overwrite 原来的previous 值
+            int[] tmp = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        //出来的时候prev 指的最新的cur，cur指的是prev, 所以结果是prev的最后一个值，不是cur的
+        return prev[prev.length - 1];
+
+
+    }
 }
