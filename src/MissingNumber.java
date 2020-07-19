f323577d71ba5a17726f7b62245a57b90b34ade7
brend <brenda.zhang2016@gmail.com>
2020-07-18 23:40:07 -0400

    MissingNumber I

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -18,6 +18,99 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 * */
 
 
+import java.util.Arrays;
+
 public class MissingNumber {
-    //todo: 把这道题集中解法总结一下
+    //way1: 暴力
+    //T: n(nlogn)
+    public int missingNumber(int[] nums) {
+        if(nums == null){
+            return -1;
+        }
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i + 1] != nums[i] + 1){
+                return nums[i] + 1;
+            }
+        }
+        if(nums[0] != 0){
+            return 0;
+        }
+        return nums[nums.length - 1] + 1;
+
+    }
+
+    //way2: 等差数列和公式
+    //T: O(n)
+    public int missingNumber1(int[] nums) {
+        //corner case:
+        if(nums == null){
+            return -1;
+        }
+        int len = nums.length;
+        //calculate sum
+        int sum = 0;
+        for(int i = 0; i < len; i++){
+            sum += nums[i];
+        }
+        return (len * (1 + len)) / 2 - sum;
+    }
+
+
+    /*way3: bit manipulation
+
+    xor 满足
+    1. 交换律： a ^ b = b ^ a
+    2. 结合律： a ^ (b ^ c) = (a ^ b) ^ c
+    3. 0 是单元： 0 ^ a = a
+
+    正常序列: 0, 1, 2, 3, 4
+    nums:    0, 1, 3, 4
+
+    正常序列每一个数[0...4]xor ^ nums 每一个数[0...4 but without 2]
+    = (0 ^ 1 ^ 2 ^ 3 ^ 4) ^ (0 ^ 1 ^ 3 ^ 4)
+    = (0 ^ 0) ^ (1 ^ 1) ^ (2) ^ (3 ^ 3) ^ (4 ^ 4) --> 交换律， 结合律
+    = 0 ^ 0 ^ 2 ^ 0 ^ 4 --> 0是单元
+    = 2 --> missing number
+
+
+    */
+    public int missingNumber2(int[] nums) {
+        if(nums == null){
+            return -1;
+        }
+        int xor = 0;
+        for(int i : nums){
+            xor ^= i;
+        }
+        for(int i = 0; i <= nums.length; i++){
+            xor ^= i;
+        }
+        return xor;
+    }
+
+    //way4
+    //hashtable (array)
+    public int missingNumber3(int[] nums) {
+        if(nums == null){
+            return -1;
+        }
+        boolean[] array = new boolean[nums.length + 1];
+        for(int i = 0; i < nums.length; i++){
+            array[nums[i]] = true;
+        }
+        for(int i = 0; i < array.length; i++){
+            if(!array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
 }
