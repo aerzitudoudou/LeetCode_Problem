e78e57d4c87bd8de5a7cc29ece2e4e247a2ea7eb
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-07-01 00:10:37 -0400

    2021_07_01

---
+++ src/LowestCommonAncestorOfaBinaryTreeIII.java
@@ -0,0 +1,23 @@
+public class LowestCommonAncestorOfaBinaryTreeIII {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    //O(h) worst case t is O(n) if it's a linkedlist, s: O(1) from xiaoganluo: https://www.youtube.com/watch?v=31KtJn5IS9Q
+    class Solution {
+        public Node lowestCommonAncestor(Node p, Node q) {
+            Node p1 = p, q1 = q;
+            while(p1 != q1){
+                p1 = p1.parent;
+                q1 = q1.parent;
+                if(p1 == null) p1 = q;
+                if(q1 == null) q1 = p;
+
+            }
+            return p1;
+
+        }
+}
