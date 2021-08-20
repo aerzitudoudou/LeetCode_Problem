1950b83e950c34f83de7c9c78a6c88b3db473619
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-08-19 20:07:40 -0400

    2021_08_20

--- src/FirstUniqueCharacterInAString_LC387.java
+++ src/FirstUniqueCharacterInAString_LC387.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FirstUniqueCharacterInAString_LC387 {
-    public int firstUniqChar(String s) {
+    public int firstUniqChar1(String s) {
         Map<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
@@ -15,6 +15,21 @@ public class FirstUniqueCharacterInAString_LC387 {
             if(map.get(c) == 1){
                 return i;
             }
+        }
+
+        return -1;
+    }
+
+    public int firstUniqChar2(String s) {
+        int[] map = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            map[c - 'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map[c - 'a'] == 1) return i;
         }
 
         return -1;
