0530d59e6211e0836ede8f1db59ddbe4ffea7898
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-09-16 23:28:15 -0400

    2021_09_16

--- src/BackPackII_Lint125.java
+++ src/BackPackII_Lint125.java
@@ -7,7 +7,7 @@ public class BackPackII_Lint125 {
      */
     public int backPackII(int m, int[] A, int[] V) {
 
-        //sol1, from acwing:https://www.acwing.com/video/34/, O(nm), O(nm)
+        //!!!sol1, from acwing:https://www.acwing.com/video/34/, O(nm), O(nm)
         if(A == null || V == null || A.length == 0 || V.length == 0 || m == 0){
             return 0;
         }
@@ -21,36 +21,48 @@ public class BackPackII_Lint125 {
                 dp[i][j] = dp[i - 1][j];
                 //take i
                 if(j - A[i - 1] >= 0) dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
-
-
             }
         }
 
         return dp[A.length][m];
     }
 
-    //todo: optimize memory to 1d dp
-    public int backPackII1(int m, int[] A, int[] V) {
+    //!!!sol2: from jh, O(nm), O(m)
+    public int backPackII2(int m, int[] A, int[] V) {
         if(A == null || V == null || A.length == 0 || V.length == 0 || m == 0){
             return 0;
         }
 
-        //i only has relation with i - 1
+        //i only has relation with i - 1, can be represented with 2 dp 1D arrays
 
         int[] dp1 = new int[m + 1]; //old
         int[] dp2 = new int[m + 1]; //new
-
         for(int i = 1; i <= A.length; i++){
             for(int j = m; j >= 0; j--){ //why big to small here? even if using dp1 and dp2
                 dp2[j] = dp1[j];
                 if(j - A[i - 1] >= 0) dp2[j] = Math.max(dp2[j], dp1[j - A[i - 1]] + V[i - 1]);
                 dp1 = dp2;
             }
-
-
         }
-
         return dp1[m];
 
+    }
+
+    //sol3, from acwing: https://www.acwing.com/video/34/ O(nm), O(m)
+    public int backPackII3(int m, int[] A, int[] V) {
+        if(A == null || V == null || A.length == 0 || V.length == 0 || m == 0){
+            return 0;
+        }
+
+        //i only has relation with i - 1
+
+        int[] dp = new int[m + 1];
+        for(int i = 1; i <= A.length; i++){
+            for(int j = m; j >= 0; j--){
+                //dp[j] = dp[j];
+                if(j - A[i - 1] >= 0) dp[j] = Math.max(dp[j], dp[j - A[i - 1]] + V[i - 1]);
+            }
+        }
+        return dp[m];
     }
 }
