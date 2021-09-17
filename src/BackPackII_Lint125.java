1c973029cd372ae140de0d2691607601bbde6d28
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-09-16 23:07:04 -0400

    2021_09_16

--- src/BackPackII_Lint125.java
+++ src/BackPackII_Lint125.java
@@ -13,7 +13,7 @@ public class BackPackII_Lint125 {
         }
 
 
-        int[][] dp = new int[A.length + 1][m + 1];
+        int[][] dp = new int[A.length + 1][m + 1]; //max with first i item having size j
         for(int i = 1; i <= A.length; i++){
             for(int j = 0; j <= m; j++){
                 int s1 = 0, s2 = 0;
@@ -30,5 +30,27 @@ public class BackPackII_Lint125 {
     }
 
     //todo: optimize memory to 1d dp
-    
+    public int backPackII1(int m, int[] A, int[] V) {
+        if(A == null || V == null || A.length == 0 || V.length == 0 || m == 0){
+            return 0;
+        }
+
+        //i only has relation with i - 1
+
+        int[] dp1 = new int[m + 1]; //old
+        int[] dp2 = new int[m + 1]; //new
+
+        for(int i = 1; i <= A.length; i++){
+            for(int j = m; j >= 0; j--){ //why big to small here? even if using dp1 and dp2
+                dp2[j] = dp1[j];
+                if(j - A[i - 1] >= 0) dp2[j] = Math.max(dp2[j], dp1[j - A[i - 1]] + V[i - 1]);
+                dp1 = dp2;
+            }
+
+
+        }
+
+        return dp1[m];
+
+    }
 }
