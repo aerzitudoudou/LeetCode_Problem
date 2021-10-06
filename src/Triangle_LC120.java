282237fc99d59a230cde98c1937b4627460c9dc2
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-10-06 19:37:35 -0400

    2021_10_06

--- src/Triangle_LC120.java
+++ src/Triangle_LC120.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Triangle_LC120 {
     /*
+    top-down
     dp[i][j] represents the minimum path sum of all paths from traiangle top to a[i - 1][j - 1]
     dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + a[i - 1][j - 1]
     res = min(dp[triangle.length][j])
@@ -37,9 +38,8 @@ public class Triangle_LC120 {
 
 
     /*
-   dp[i][j] represents the minimum path sum of all paths from traiangle top to a[i - 1][j - 1]
-   dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + a[i - 1][j - 1]
-   res = min(dp[triangle.length][j])
+
+    top-down, reduce space dimension by 1
    */
     //sol2, my, O(n^2), O(n)
     public int minimumTotal2(List<List<Integer>> triangle) {
@@ -73,4 +73,61 @@ public class Triangle_LC120 {
         }
         return res;
     }
+
+    //sol3, bottom up, O(n^2), O(n^2)
+    //dp[i][j] represent the min path sum from all path from points in bottom level of triangle till current point  a[i - 1][j - 1]
+    public int minimumTotal3(List<List<Integer>> triangle) {
+        int res = Integer.MAX_VALUE;
+        int n = triangle.size();
+        int[][] dp = new int[n + 1][n + 1];
+        //init
+        for(int i = 0; i < n + 1; i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                if(i == n){
+                    dp[i][j] = triangle.get(i - 1).get(j - 1);
+                }else{
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i - 1).get(j - 1);
+                }
+
+            }
+        }
+
+
+        return dp[1][1];
+    }
+
+    //sol4, decrease space dimensions by 1 from sol3
+    public int minimumTotal4(List<List<Integer>> triangle) {
+        int res = Integer.MAX_VALUE;
+        int n = triangle.size();
+        int[] dp1 = new int[n + 1];
+        //init
+
+        Arrays.fill(dp1, Integer.MAX_VALUE);
+
+
+        for(int i = n; i >= 1; i--){
+            int[] dp2 = new int[n + 1];
+            Arrays.fill(dp2, Integer.MAX_VALUE);
+            for(int j = 1; j <= i; j++){
+                if(i == n){
+                    dp2[j] = triangle.get(i - 1).get(j - 1);
+                }else{
+                    dp2[j] = Math.min(dp1[j], dp1[j + 1]) + triangle.get(i - 1).get(j - 1);
+                }
+
+            }
+            dp1 = dp2;
+        }
+
+
+        return dp1[1];
+    }
+
+
+
 }
