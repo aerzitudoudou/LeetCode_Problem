899a705c18bc34e46f7dca4ff1624164a733db4c
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-12-01 22:17:09 -0500

    2021_12_1

---
+++ src/RecoverBinarySearchTree_LC99.java
@@ -0,0 +1,33 @@
+public class RecoverBinarySearchTree_LC99 {
+    //in order traversal, find a1 > a2 && a3 > a4. a1 a4 is the node to be switched
+    //corner case: only have a1 > a2, switch
+    TreeNode first = null, second = null, lastSeen = null;
+    public void recoverTree(TreeNode root) {
+
+        dfs(root);
+        int tmp = first.key;
+        first.key = second.key;
+        second.key = tmp;
+    }
+
+
+    private void dfs(TreeNode root){
+        if(root == null) return;
+        //in order traversal
+        dfs(root.left);
+
+
+        if(lastSeen != null && root.key < lastSeen.key){
+            second = root;
+            if(first == null){
+                first = lastSeen;
+            }else{
+                return;
+            }
+        }
+        lastSeen = root;
+
+
+        dfs(root.right);
+    }
+}
