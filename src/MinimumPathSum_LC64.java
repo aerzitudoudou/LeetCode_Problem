16db444ab35b3862d3e610d7ee195b0b67e17a31
aerzitudoudou <brenda.zhang2016@gmail.com>
2021-12-07 12:34:12 -0500

    2021_12_

--- src/MinimumPathSum_LC64.java
+++ src/MinimumPathSum_LC64.java
@@ -48,4 +48,29 @@ public class MinimumPathSum_LC64 {
         return dp1[n];
 
     }
+
+    //sol3, re-use original grid, O(mn), O(1)
+    public int minPathSum3(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    continue;
+                }else if(i == 0){
+                    grid[i][j] = grid[i][j - 1] + grid[i][j];
+                }else if(j == 0){
+                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                }else{
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+
 }
