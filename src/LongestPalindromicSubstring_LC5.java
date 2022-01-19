d4122ce9b6fba637e2310a0278a94cf649d6102d
aerzitudoudou <brenda.zhang2016@gmail.com>
2022-01-18 23:51:28 -0500

    2022_01_18

--- src/LongestPalindromicSubstring_LC5.java
+++ src/LongestPalindromicSubstring_LC5.java
@@ -4,7 +4,7 @@ public class LongestPalindromicSubstring_LC5 {
     cbcbbcbd
           j
     */
-    //from huahua, https://www.youtube.com/watch?v=g3R-pjUNa3k O(n^2), O(1)
+    //sol1, from huahua, https://www.youtube.com/watch?v=g3R-pjUNa3k O(n^2), O(1)
     public String longestPalindrome(String s) {
         int start = 0, len = 0;
         for(int i = 0; i < s.length(); i++){
@@ -30,5 +30,36 @@ public class LongestPalindromicSubstring_LC5 {
         }
         //end-start + 1 - 2 since the left and right boundary are not valid chars
         return end - start - 1;
+    }
+
+
+    //!!!sol2, my, 中心开花， 分奇数偶数讨论， O(n^2), O(1)
+    public String longestPalindrome2(String s) {
+        String res = "";
+        int len = s.length();
+        //baab  baacaab
+        //odd (l, r)
+        for(int i = 0; i < len; i++){
+            res = calculateLen2(i, i, res, s);
+        }
+
+        //even (l, r)
+        for(int i = 1; i < len; i++){
+            res = calculateLen2(i - 1, i, res, s);
+        }
+
+        return res;
+    }
+
+    private String calculateLen2(int l, int r, String res, String s){
+        int len = s.length();
+        while(l >= 0 && r < len && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+        }
+        if(r - l - 1 > res.length()){
+            res = s.substring(l + 1, r);
+        }
+        return res;
     }
 }
