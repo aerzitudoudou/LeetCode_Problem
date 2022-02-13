7ea66b1310466ef5c74e89f1ee80d8d5438498e5
aerzitudoudou <brenda.zhang2016@gmail.com>
2022-02-13 12:31:53 -0500

    2022_02_12

--- src/IntersectionOfTwoLinkedLIsts_LC160.java
+++ src/IntersectionOfTwoLinkedLIsts_LC160.java
@@ -1,5 +1,5 @@
 public class IntersectionOfTwoLinkedLIsts_LC160 {
-    //sol1, my, O(n), O(1)
+    //sol1, my, O(n + m), O(1)
     //1, Get the length of the two lists.
     //
     //2, Align them to the same start point.
@@ -34,5 +34,23 @@ public class IntersectionOfTwoLinkedLIsts_LC160 {
             head = head.next;
         }
         return len;
+    }
+
+
+    //!!!!!sol2, physical prob, O(n), O(1)
+    //                    a1
+    //A              B-------
+    //             a2
+    //           ----
+    //                b
+    //B          ----A-------
+    //Va = Vb, when b finishes, a must be finish the a2 part at B. at this point move b from finish line to starting of a1 garantees A and B are moving from the same position in parallel.
+    public ListNode getIntersectionNode2(ListNode a, ListNode b) {
+        ListNode startA = a, startB = b;
+        while(a != b){ //if no intersection, then a == b == null also satisfies
+            a = (a == null) ? startB : a.next;
+            b = (b == null) ? startA : b.next;
+        }
+        return a;
     }
 }
