a55d99c2a12248bc2895884e73ac87434dc51cc2
aerzitudoudou <brenda.zhang2016@gmail.com>
2022-04-25 23:50:52 -0400

    2022_03_13

--- src/Main.java
+++ src/Main.java
@@ -859,9 +859,10 @@ public class Main {
 //        list.add(2);
 //        Integer[] ary =  list.toArray(new Integer[list.size()]);
 //        System.out.println(ary[0] + "," + ary[1]);
-        SortColorsII_Lint143 sortColorsII_lint143 = new SortColorsII_Lint143();
-        sortColorsII_lint143.sortColors2(new int[] {2,1,1,2,2, 3,4,5,3,3,3,3,4,4,4,4,2,1,5}, 5);
-
+//        SortColorsII_Lint143 sortColorsII_lint143 = new SortColorsII_Lint143();
+//        sortColorsII_lint143.sortColors2(new int[] {2,1,1,2,2, 3,4,5,3,3,3,3,4,4,4,4,2,1,5}, 5);
+        ThreeSum threeSum = new ThreeSum();
+        threeSum.threeSum(new int[]{-1,0,1,2,-1,4});
 
 
 
