4c7b30247013fd647d44c802263655ea7f8accb8
aerzitudoudou <brenda.zhang2016@gmail.com>
2022-05-03 22:05:44 -0400

    2022_05_03

--- src/ConvertBinarySearchTreeToSortedDoublyLinkedList_LC426.java
+++ src/ConvertBinarySearchTreeToSortedDoublyLinkedList_LC426.java
@@ -23,73 +23,33 @@ Null indicates the absence of a value, so there is no object on which the reques
     * */
     // int a = 0; for primitive & change of reference instead of de-reference of the object reference is pointing to, make them either global or put them in a container class
 
-    //Sol1
-    //O(n), O(h)
+
+    //!!Sol1, my, O(n), O(h)
+    Node head = null; //global head of the half before current root
+    Node tail = null; //global tail of the half before current root
     public Node treeToDoublyList(Node root) {
-        Node[] head = new Node[1];
-        Node[] pre = new Node[1];
-//     StringBuilder sb = new StringBuilder();
-
-//     sb  --> object       head --> object
-
-//     sb1                  head1 = root is not change the original object head is pointing to
-//     sb.append(a)         head1.val = 3  de-reference head and change value in the node
-
-//      int[] a
-//         a[0] is like a.0 =
-        dfs(root, head, pre);
-        if(head[0] == null) return head[0];
-
-
-        head[0].left = pre[0];
-        pre[0].right = head[0];
-        return head[0];
-
+        if(root == null) return null;
+        dfs(root);
+        head.left = tail;
+        tail.right = head;
+        return head;
 
     }
-    private void dfs(Node root, Node[] head, Node[] pre){
 
+    //BST, in-order traverse to make the result ordered
+    private void dfs(Node root){
         if(root == null) return;
-        dfs(root.left, head, pre);
-        //do something on the current level
-        if(head[0] == null){
-            head[0] = root;
-        }else{
-            pre[0].right = root;
-            root.left = pre[0];
+
+        dfs(root.left);
+        if(head == null) head = root;//bottom left node. i.e. smallest node
+        else{
+            root.left = tail;
+            tail.right = root;
         }
-        pre[0] = root;
-        dfs(root.right, head, pre);
+        tail = root;
+        dfs(root.right);
 
     }
-
-
-    //！！！！！sol2: make head and pre as a global variable to be able to update it in the dfs
-    //O(n), O(h)
-//      Node head = null, pre = null;
-//      public Node treeToDoublyList(Node root) {
-//         dfs(root);
-//         if(head == null) return head;
-//         head.left = pre;
-//         pre.right = head;
-//         return head;
-
-
-//     }
-//     private void dfs(Node root){
-//         if(root == null) return;
-//         dfs(root.left);
-//         //do something on the current level
-//         if(head == null){
-//             head = root;
-//         }else{
-//             pre.right = root;
-//             root.left = pre;
-//         }
-//         pre = root;
-//         dfs(root.right);
-
-//     }
 
 
 
