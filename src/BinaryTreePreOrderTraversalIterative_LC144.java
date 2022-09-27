f235624d2c79e945ed00bd5dd511160f25cb3718
aerzitudoudou <brenda.zhang2016@gmail.com>
2022-09-26 21:30:03 -0400

    2022_09_26

--- src/BinaryTreePreOrderTraversalIterative_LC144.java
+++ src/BinaryTreePreOrderTraversalIterative_LC144.java
@@ -3,23 +3,18 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
-public class BinaryTreePreOrderTraversalIterative {
+public class BinaryTreePreOrderTraversalIterative_LC144 {
+    //sol1, my, O(n), O(h)
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if(root == null){
-            return res;
-        }
+        if(root == null) return res;
         Deque<TreeNode> stack = new LinkedList<>();
         stack.offerFirst(root);
         while(!stack.isEmpty()){
             TreeNode cur = stack.pollFirst();
             res.add(cur.key);
-            if(cur.right != null){
-                stack.offerFirst(cur.right);
-            }
-            if(cur.left != null){
-                stack.offerFirst(cur.left);
-            }
+            if(cur.right != null) stack.offerFirst(cur.right);
+            if(cur.left != null) stack.offerFirst(cur.left);
         }
         return res;
     }
